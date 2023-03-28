a5eaff6c0510c1c387ec9e8283ce447afd50f24f
J-E <beillevertj.contact@gmail.com>
2023-03-28 14:08:11 +0200

    Quete3 terminée: Affichage variable

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[1];
+
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>bulbasaur</figcaption>
+            {pokemon.hasOwnProperty('imgSrc') ? <img src={pokemon.imgSrc}/> : <p>???</p> }
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-    )
+    ) 
 }
+
 
 export default PokemonCard;
