b94b3ff045ce86ce81e4b5663875d599dbefc7bc
J-E <beillevertj.contact@gmail.com>
2023-03-29 11:48:27 +0200

    Quete 5 terminée

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,14 @@
+import PropTypes from "prop-types"
 
 
 function PokemonCard(props) {
+
+    PokemonCard.propTypes = {
+        pokemon: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            imgSrc: PropTypes.string,
+        }).isRequired,
+    }
 
     return (
         <figure>
